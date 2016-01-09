f1339849b2a25d13cbe3ed678c28dc37dc77c3ed
Staffan Gustafsson <info@powercode-consulting.se>
2016-01-09 19:38:16 +0100

    Moving DebuggerProxy to a separate file

--- PSExtCmdlets/DebuggerDispatcher.cs
+++ PSExtCmdlets/DebuggerDispatcher.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Dynamic;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
@@ -152,82 +150,4 @@ namespace PSExt
 		private static DebuggerDispatcher _instance;
 		public static DebuggerDispatcher Instance => _instance ?? (_instance = new DebuggerDispatcher());
 	}
-
-	public class DebuggerProxy : IDebugger
-	{		
-		private readonly dynamic _proxy;
-		public DebuggerProxy(IDebugger debugger)
-		{
-			_proxy = new DynamicDebuggerProxy(debugger);
-		}
-
-		public string ExecuteCommand(string command)
-		{
-			return _proxy.ExecuteCommand(command);
-		}
-
-		public string ReadLine()
-		{
-			return _proxy.ReadLine();
-		}
-
-		public void Write(string value)
-		{
-			_proxy.Write(value);
-		}
-
-		public IList<BreakpointData> GetBreakpoints()
-		{
-			return _proxy.GetBreakpoints();
-		}
-
-		public IList<BreakpointData> AddBreakpoints(BreakpointData data)
-		{
-			return _proxy.AddBreakpoints(data);
-		}
-
-		public IList<ModuleData> GetModules()
-		{
-			return _proxy.GetModules();
-		}
-
-		public Callstack GetCallstack()
-		{
-			return _proxy.GetCallstack();
-		}
-	
-	}
-
-	class DynamicDebuggerProxy: DynamicObject
-	{
-		private readonly IDebugger _proxy;
-		private readonly DebuggerDispatcher _dispatcher;
-
-		public DynamicDebuggerProxy(IDebugger proxy)
-		{
-			_proxy = proxy;
-			_dispatcher = DebuggerDispatcher.Instance;
-		}
-
-		public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
-		{
-			try
-			{
-				var mi = typeof(IDebugger).GetMethod(binder.Name);
-				if (_dispatcher.DispatchRequired())
-				{
-					result = _dispatcher.InvokeFunction(new MethodInvocationInfo(mi, _proxy, args));
-					return true;
-				}
-				result = mi.Invoke(_proxy, args);
-				return true;
-			}
-			catch
-			{
-				result = null;
-				return false;
-			}
-		}
-	}
-
 }
---
+++ PSExtCmdlets/DebuggerProxy.cs
@@ -0,0 +1,91 @@
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace PSExt
+{
+	/// <summary>
+	/// Proxy to debugger responsible for invoking the calls from PowerShell on the debugger thread.
+	/// DynamicDebuggerProxy helper does this and delegates to the real debugger.
+	/// </summary>
+	public class DebuggerProxy : IDebugger
+	{		
+		private readonly dynamic _proxy;
+
+		/// <summary>
+		/// Creates a new instance of the debugger proxy
+		/// </summary>
+		/// <param name="debugger">the real native debugger to delegate calls to</param>
+		public DebuggerProxy(IDebugger debugger)
+		{
+			_proxy = new DynamicDebuggerProxy(debugger);
+		}
+
+		public string ExecuteCommand(string command)
+		{
+			return _proxy.ExecuteCommand(command);
+		}
+
+		public string ReadLine()
+		{
+			return _proxy.ReadLine();
+		}
+
+		public void Write(string value)
+		{
+			_proxy.Write(value);
+		}
+
+		public IList<BreakpointData> GetBreakpoints()
+		{
+			return _proxy.GetBreakpoints();
+		}
+
+		public IList<BreakpointData> AddBreakpoints(BreakpointData data)
+		{
+			return _proxy.AddBreakpoints(data);
+		}
+
+		public IList<ModuleData> GetModules()
+		{
+			return _proxy.GetModules();
+		}
+
+		public Callstack GetCallstack()
+		{
+			return _proxy.GetCallstack();
+		}
+
+		class DynamicDebuggerProxy : DynamicObject
+		{
+			private readonly IDebugger _proxy;
+			private readonly DebuggerDispatcher _dispatcher;
+
+			public DynamicDebuggerProxy(IDebugger proxy)
+			{
+				_proxy = proxy;
+				_dispatcher = DebuggerDispatcher.Instance;
+			}
+
+			public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+			{
+				try
+				{
+					var mi = typeof(IDebugger).GetMethod(binder.Name);
+					if (_dispatcher.DispatchRequired())
+					{
+						result = _dispatcher.InvokeFunction(new MethodInvocationInfo(mi, _proxy, args));
+						return true;
+					}
+					result = mi.Invoke(_proxy, args);
+					return true;
+				}
+				catch
+				{
+					result = null;
+					return false;
+				}
+			}
+		}
+
+	}
+}
