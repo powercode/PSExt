eb9706f989b7992f54e576d58061340a06785f50
Staffan Gustafsson <info@powercode-consulting.se>
2016-01-11 00:38:57 +0100

    Removing unmanged parts and renaming C# project to PSExt.

--- PSExt/NativeModule.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <windows.h>
-#include "nowarn/dbghelp.h"
-#include <vector>
-
-
-using ModuleVector = std::vector<_IMAGEHLP_MODULEW64>;
-ModuleVector GetNativeModules();
--- PSExt/nowarn/dbgeng.h
+++
@@ -1,6 +0,0 @@
-
-#pragma warning (push)
-#pragma warning (disable:4838 4091)
-#include <DbgEng.h>
-
-#pragma warning (pop)
