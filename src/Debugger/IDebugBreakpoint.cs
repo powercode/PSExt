50ce3d56ab62ab393308449e43218f619884015f
Staffan Gustafsson <info@powercode-consulting.se>
2016-01-13 00:44:39 +0100

    Resharper formatting

--- src/Debugger/IDebugBreakpoint.cs
+++ src/Debugger/IDebugBreakpoint.cs
@@ -1,109 +1,108 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Text;
 using System.Runtime.InteropServices;
+using System.Text;
 
 #pragma warning disable 1591
 
 namespace Microsoft.Diagnostics.Runtime.Interop
 {
-    [ComImport, InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("5bd9d474-5975-423a-b88b-65a8e7110e65")]
-    public interface IDebugBreakpoint
-    {
-        /* IDebugBreakpoint */
+	[ComImport, InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("5bd9d474-5975-423a-b88b-65a8e7110e65")]
+	public interface IDebugBreakpoint
+	{
+		/* IDebugBreakpoint */
 
-        [PreserveSig]
-        int GetId(
-            [Out] out UInt32 Id);
+		[PreserveSig]
+		int GetId(
+			[Out] out uint Id);
 
-        [PreserveSig]
-        int GetType(
-            [Out] out DEBUG_BREAKPOINT_TYPE BreakType,
-            [Out] out UInt32 ProcType);
+		[PreserveSig]
+		int GetType(
+			[Out] out DEBUG_BREAKPOINT_TYPE BreakType,
+			[Out] out uint ProcType);
 
-        //FIX ME!!! Should try and get an enum for this
-        [PreserveSig]
-        int GetAdder(
-            [Out, MarshalAs(UnmanagedType.Interface)] out IDebugClient Adder);
+		//FIX ME!!! Should try and get an enum for this
+		[PreserveSig]
+		int GetAdder(
+			[Out, MarshalAs(UnmanagedType.Interface)] out IDebugClient Adder);
 
-        [PreserveSig]
-        int GetFlags(
-            [Out] out DEBUG_BREAKPOINT_FLAG Flags);
+		[PreserveSig]
+		int GetFlags(
+			[Out] out DEBUG_BREAKPOINT_FLAG Flags);
 
-        [PreserveSig]
-        int AddFlags(
-            [In] DEBUG_BREAKPOINT_FLAG Flags);
+		[PreserveSig]
+		int AddFlags(
+			[In] DEBUG_BREAKPOINT_FLAG Flags);
 
-        [PreserveSig]
-        int RemoveFlags(
-            [In] DEBUG_BREAKPOINT_FLAG Flags);
+		[PreserveSig]
+		int RemoveFlags(
+			[In] DEBUG_BREAKPOINT_FLAG Flags);
 
-        [PreserveSig]
-        int SetFlags(
-            [In] DEBUG_BREAKPOINT_FLAG Flags);
+		[PreserveSig]
+		int SetFlags(
+			[In] DEBUG_BREAKPOINT_FLAG Flags);
 
-        [PreserveSig]
-        int GetOffset(
-            [Out] out UInt64 Offset);
+		[PreserveSig]
+		int GetOffset(
+			[Out] out ulong Offset);
 
-        [PreserveSig]
-        int SetOffset(
-            [In] UInt64 Offset);
+		[PreserveSig]
+		int SetOffset(
+			[In] ulong Offset);
 
-        [PreserveSig]
-        int GetDataParameters(
-            [Out] out UInt32 Size,
-            [Out] out DEBUG_BREAKPOINT_ACCESS_TYPE AccessType);
+		[PreserveSig]
+		int GetDataParameters(
+			[Out] out uint Size,
+			[Out] out DEBUG_BREAKPOINT_ACCESS_TYPE AccessType);
 
-        [PreserveSig]
-        int SetDataParameters(
-            [In] UInt32 Size,
-            [In] DEBUG_BREAKPOINT_ACCESS_TYPE AccessType);
+		[PreserveSig]
+		int SetDataParameters(
+			[In] uint Size,
+			[In] DEBUG_BREAKPOINT_ACCESS_TYPE AccessType);
 
-        [PreserveSig]
-        int GetPassCount(
-            [Out] out UInt32 Count);
+		[PreserveSig]
+		int GetPassCount(
+			[Out] out uint Count);
 
-        [PreserveSig]
-        int SetPassCount(
-            [In] UInt32 Count);
+		[PreserveSig]
+		int SetPassCount(
+			[In] uint Count);
 
-        [PreserveSig]
-        int GetCurrentPassCount(
-            [Out] out UInt32 Count);
+		[PreserveSig]
+		int GetCurrentPassCount(
+			[Out] out uint Count);
 
-        [PreserveSig]
-        int GetMatchThreadId(
-            [Out] out UInt32 Id);
+		[PreserveSig]
+		int GetMatchThreadId(
+			[Out] out uint Id);
 
-        [PreserveSig]
-        int SetMatchThreadId(
-            [In] UInt32 Thread);
+		[PreserveSig]
+		int SetMatchThreadId(
+			[In] uint Thread);
 
-        [PreserveSig]
-        int GetCommand(
-            [Out, MarshalAs(UnmanagedType.LPStr)] StringBuilder Buffer,
-            [In] Int32 BufferSize,
-            [Out] out UInt32 CommandSize);
+		[PreserveSig]
+		int GetCommand(
+			[Out, MarshalAs(UnmanagedType.LPStr)] StringBuilder Buffer,
+			[In] int BufferSize,
+			[Out] out uint CommandSize);
 
-        [PreserveSig]
-        int SetCommand(
-            [In, MarshalAs(UnmanagedType.LPStr)] string Command);
+		[PreserveSig]
+		int SetCommand(
+			[In, MarshalAs(UnmanagedType.LPStr)] string Command);
 
-        [PreserveSig]
-        int GetOffsetExpression(
-            [Out, MarshalAs(UnmanagedType.LPStr)] StringBuilder Buffer,
-            [In] Int32 BufferSize,
-            [Out] out UInt32 ExpressionSize);
+		[PreserveSig]
+		int GetOffsetExpression(
+			[Out, MarshalAs(UnmanagedType.LPStr)] StringBuilder Buffer,
+			[In] int BufferSize,
+			[Out] out uint ExpressionSize);
 
-        [PreserveSig]
-        int SetOffsetExpression(
-            [In, MarshalAs(UnmanagedType.LPStr)] string Expression);
+		[PreserveSig]
+		int SetOffsetExpression(
+			[In, MarshalAs(UnmanagedType.LPStr)] string Expression);
 
-        [PreserveSig]
-        int GetParameters(
-            [Out] out DEBUG_BREAKPOINT_PARAMETERS Params);
-    }
+		[PreserveSig]
+		int GetParameters(
+			[Out] out DEBUG_BREAKPOINT_PARAMETERS Params);
+	}
 }
--- src/Debugger/IDebugBreakpoint3.cs
+++ src/Debugger/IDebugBreakpoint3.cs
@@ -2,135 +2,135 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Text;
 using System.Runtime.InteropServices;
+using System.Text;
 
 #pragma warning disable 1591
 
 namespace Microsoft.Diagnostics.Runtime.Interop
 {
-    [ComImport, InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("38f5c249-b448-43bb-9835-579d4ec02249")]
-    public interface IDebugBreakpoint3 : IDebugBreakpoint2
-    {
-        /* IDebugBreakpoint */
+	[ComImport, InterfaceType(ComInterfaceType.InterfaceIsIUnknown), Guid("38f5c249-b448-43bb-9835-579d4ec02249")]
+	public interface IDebugBreakpoint3 : IDebugBreakpoint2
+	{
+		/* IDebugBreakpoint */
 
-        [PreserveSig]
-        new int GetId(
-            [Out] out UInt32 Id);
+		[PreserveSig]
+		new int GetId(
+			[Out] out uint Id);
 
-        [PreserveSig]
-        new int GetType(
-            [Out] out DEBUG_BREAKPOINT_TYPE BreakType,
-            [Out] out UInt32 ProcType);
+		[PreserveSig]
+		new int GetType(
+			[Out] out DEBUG_BREAKPOINT_TYPE BreakType,
+			[Out] out uint ProcType);
 
-        //FIX ME!!! Should try and get an enum for this
-        [PreserveSig]
-        new int GetAdder(
-            [Out, MarshalAs(UnmanagedType.Interface)] out IDebugClient Adder);
+		//FIX ME!!! Should try and get an enum for this
+		[PreserveSig]
+		new int GetAdder(
+			[Out, MarshalAs(UnmanagedType.Interface)] out IDebugClient Adder);
 
-        [PreserveSig]
-        new int GetFlags(
-            [Out] out DEBUG_BREAKPOINT_FLAG Flags);
+		[PreserveSig]
+		new int GetFlags(
+			[Out] out DEBUG_BREAKPOINT_FLAG Flags);
 
-        [PreserveSig]
-        new int AddFlags(
-            [In] DEBUG_BREAKPOINT_FLAG Flags);
+		[PreserveSig]
+		new int AddFlags(
+			[In] DEBUG_BREAKPOINT_FLAG Flags);
 
-        [PreserveSig]
-        new int RemoveFlags(
-            [In] DEBUG_BREAKPOINT_FLAG Flags);
+		[PreserveSig]
+		new int RemoveFlags(
+			[In] DEBUG_BREAKPOINT_FLAG Flags);
 
-        [PreserveSig]
-        new int SetFlags(
-            [In] DEBUG_BREAKPOINT_FLAG Flags);
+		[PreserveSig]
+		new int SetFlags(
+			[In] DEBUG_BREAKPOINT_FLAG Flags);
 
-        [PreserveSig]
-        new int GetOffset(
-            [Out] out UInt64 Offset);
+		[PreserveSig]
+		new int GetOffset(
+			[Out] out ulong Offset);
 
-        [PreserveSig]
-        new int SetOffset(
-            [In] UInt64 Offset);
+		[PreserveSig]
+		new int SetOffset(
+			[In] ulong Offset);
 
-        [PreserveSig]
-        new int GetDataParameters(
-            [Out] out UInt32 Size,
-            [Out] out DEBUG_BREAKPOINT_ACCESS_TYPE AccessType);
+		[PreserveSig]
+		new int GetDataParameters(
+			[Out] out uint Size,
+			[Out] out DEBUG_BREAKPOINT_ACCESS_TYPE AccessType);
 
-        [PreserveSig]
-        new int SetDataParameters(
-            [In] UInt32 Size,
-            [In] DEBUG_BREAKPOINT_ACCESS_TYPE AccessType);
+		[PreserveSig]
+		new int SetDataParameters(
+			[In] uint Size,
+			[In] DEBUG_BREAKPOINT_ACCESS_TYPE AccessType);
 
-        [PreserveSig]
-        new int GetPassCount(
-            [Out] out UInt32 Count);
+		[PreserveSig]
+		new int GetPassCount(
+			[Out] out uint Count);
 
-        [PreserveSig]
-        new int SetPassCount(
-            [In] UInt32 Count);
+		[PreserveSig]
+		new int SetPassCount(
+			[In] uint Count);
 
-        [PreserveSig]
-        new int GetCurrentPassCount(
-            [Out] out UInt32 Count);
+		[PreserveSig]
+		new int GetCurrentPassCount(
+			[Out] out uint Count);
 
-        [PreserveSig]
-        new int GetMatchThreadId(
-            [Out] out UInt32 Id);
+		[PreserveSig]
+		new int GetMatchThreadId(
+			[Out] out uint Id);
 
-        [PreserveSig]
-        new int SetMatchThreadId(
-            [In] UInt32 Thread);
+		[PreserveSig]
+		new int SetMatchThreadId(
+			[In] uint Thread);
 
-        [PreserveSig]
-        new int GetCommand(
-            [Out, MarshalAs(UnmanagedType.LPStr)] StringBuilder Buffer,
-            [In] Int32 BufferSize,
-            [Out] out UInt32 CommandSize);
+		[PreserveSig]
+		new int GetCommand(
+			[Out, MarshalAs(UnmanagedType.LPStr)] StringBuilder Buffer,
+			[In] int BufferSize,
+			[Out] out uint CommandSize);
 
-        [PreserveSig]
-        new int SetCommand(
-            [In, MarshalAs(UnmanagedType.LPStr)] string Command);
+		[PreserveSig]
+		new int SetCommand(
+			[In, MarshalAs(UnmanagedType.LPStr)] string Command);
 
-        [PreserveSig]
-        new int GetOffsetExpression(
-            [Out, MarshalAs(UnmanagedType.LPStr)] StringBuilder Buffer,
-            [In] Int32 BufferSize,
-            [Out] out UInt32 ExpressionSize);
+		[PreserveSig]
+		new int GetOffsetExpression(
+			[Out, MarshalAs(UnmanagedType.LPStr)] StringBuilder Buffer,
+			[In] int BufferSize,
+			[Out] out uint ExpressionSize);
 
-        [PreserveSig]
-        new int SetOffsetExpression(
-            [In, MarshalAs(UnmanagedType.LPStr)] string Expression);
+		[PreserveSig]
+		new int SetOffsetExpression(
+			[In, MarshalAs(UnmanagedType.LPStr)] string Expression);
 
-        [PreserveSig]
-        new int GetParameters(
-            [Out] out DEBUG_BREAKPOINT_PARAMETERS Params);
+		[PreserveSig]
+		new int GetParameters(
+			[Out] out DEBUG_BREAKPOINT_PARAMETERS Params);
 
-        /* IDebugBreakpoint2 */
+		/* IDebugBreakpoint2 */
 
-        [PreserveSig]
-        new int GetCommandWide(
-            [Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder Buffer,
-            [In] Int32 BufferSize,
-            [Out] out UInt32 CommandSize);
+		[PreserveSig]
+		new int GetCommandWide(
+			[Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder Buffer,
+			[In] int BufferSize,
+			[Out] out uint CommandSize);
 
-        [PreserveSig]
-        new int SetCommandWide(
-            [In, MarshalAs(UnmanagedType.LPWStr)] string Command);
+		[PreserveSig]
+		new int SetCommandWide(
+			[In, MarshalAs(UnmanagedType.LPWStr)] string Command);
 
-        [PreserveSig]
-        new int GetOffsetExpressionWide(
-            [Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder Buffer,
-            [In] Int32 BufferSize,
-            [Out] out UInt32 ExpressionSize);
+		[PreserveSig]
+		new int GetOffsetExpressionWide(
+			[Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder Buffer,
+			[In] int BufferSize,
+			[Out] out uint ExpressionSize);
 
-        [PreserveSig]
-        new int SetOffsetExpressionWide(
-            [In, MarshalAs(UnmanagedType.LPWStr)] string Command);
+		[PreserveSig]
+		new int SetOffsetExpressionWide(
+			[In, MarshalAs(UnmanagedType.LPWStr)] string Command);
 
-        /* IDebugBreakpoint3 */
+		/* IDebugBreakpoint3 */
 
-        [PreserveSig]
-        int GetGuid([Out] out Guid Guid);
-    }
+		[PreserveSig]
+		int GetGuid([Out] out Guid Guid);
+	}
 }
